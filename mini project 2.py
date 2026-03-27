bac5e34bb2db7ad85c320151dfb303d92b0a26dc
zak90847 <zs251@kent.ac.uk>
2026-03-27 11:53:10 +0000

    14

--- mini project 2.py
+++ mini project 2.py
@@ -47,3 +47,8 @@ def save_balance(balance)
               f.write(str(balance))
 
 def play_game:
+    balance = load_balance(
+    player = Player ("User", balance)
+    deck = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'] * 4
+    random.shuffle(deck)
+
