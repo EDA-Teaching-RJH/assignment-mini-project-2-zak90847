7f43d99fc04aa88b73d63b2941eaaf64aee9fa8a
zak90847 <zs251@kent.ac.uk>
2026-03-27 11:27:40 +0000

    6

--- mini project 2.py
+++ mini project 2.py
@@ -6,4 +6,11 @@ class Participant:
     def __init__(self, name):
         self.name = name
         self.hand = []
+
+        def add_card(self, card):
+            self.hand.append(card)
         
+        def get_score(self):
+            score = 0
+            aces = 0
+            
