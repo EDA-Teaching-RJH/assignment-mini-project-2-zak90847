de28012a376cd420d175319f32c3fde5df95a49a
zak90847 <zs251@kent.ac.uk>
2026-03-27 11:25:34 +0000

    5

--- mini project 2.py
+++ mini project 2.py
@@ -1,20 +1,9 @@
-import re
 import random
+import os
+from validators import  validate_bet, validate_action
 
 class Participant:
-    def __init(self,name):
+    def __init__(self, name):
         self.name = name
         self.hand = []
-        self.score = 0
-
-    def add_card(self, card):
-        self.hand.append(card)
-
-class Player(Participant):
-    def __init__(self, name, balance=100):
-        super().__init__(name)
-        self.balance = balance
-
-class Dealer(Participant):
-    def __init__(self):
-        super().__init__("Dealer")
+        
